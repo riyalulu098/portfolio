652f466dc0f64a078d5fe1b461f89273eb3ecd45
riyalulu098 <riyalulu098@gmail.com>
2026-03-12 14:13:42 +0530

    Fix router for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 
 // Lazy loading for optimization
